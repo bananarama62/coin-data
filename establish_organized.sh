3b3c654bc5e2a145d9564d5b86bff3c3685048b7
JMGillum <JMGillum@protonmail.com>
2026-01-19 02:37:24 -0800

    Two scripts that are useful for creating backups for changes

---
+++ establish_organized.sh
@@ -0,0 +1,7 @@
+mv setup_countries.sql setup_countries.sql.bak
+mv setup_denominations.sql setup_denominations.sql.bak
+mv setup_values.sql setup_values.sql.bak
+
+mv setup_countries.sql.organized setup_countries.sql
+mv setup_denominations.sql.organized setup_denominations.sql
+mv setup_values.sql.organized setup_values.sql
---
+++ restore_backup.sh
@@ -0,0 +1,3 @@
+mv setup_countries.sql.bak setup_countries.sql
+mv setup_denominations.sql.bak setup_denominations.sql
+mv setup_values.sql.bak setup_values.sql
