46866b93547ecf183093773b8f0cc2a1ef0d8d23
JMGillum <JMGillum@protonmail.com>
2026-02-09 18:02:45 -0800

    Deleted obsolete files

--- merge.bat
+++
@@ -1,6 +0,0 @@
-del setup.sql
-type setup_db.sql >> setup.sql
-type setup_countries.sql >> setup.sql
-type setup_denominations.sql >> setup.sql
-type setup_values.sql >> setup.sql
-type setup_coins.sql >> setup.sql
--- restore_backup.sh
+++
@@ -1,3 +0,0 @@
-mv setup_countries.sql.bak setup_countries.sql
-mv setup_denominations.sql.bak setup_denominations.sql
-mv setup_values.sql.bak setup_values.sql
